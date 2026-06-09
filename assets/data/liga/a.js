7a05daa1bdb1d5c3f07e7469c2c27d8caeb8c8ca
Brenno Thiago <167652253+Brennoth@users.noreply.github.com>
2026-06-08 22:33:12 -0300

    liga

---
+++ assets/data/liga/a.js
@@ -0,0 +1,449 @@
+//jogadores
+
+{
+    "season1": {
+
+      "data": "10/06/2026",
+      "equipes": {
+        
+        "FCB E-SPORTS": {
+          "《ŁĐ》WHITER™": {
+            "logo": "",
+            "dados": [0, 0, 2]
+          },
+          "《ŁĐ》DG777": {
+            "logo": "",
+            "dados": [0, 0, 1]
+          },
+          "TKX  NOBREmvp": {
+            "logo": "",
+            "dados": [1, 1, 0]
+          },
+          "LD  JRzin⁶⁶⁶": {
+            "logo": "",
+            "dados": [0, null, null]
+          },
+          "sub-~-yuri": {
+            "logo": "",
+            "dados": [null, 1, 2]
+          }
+        },
+        "SERROCITY": {
+          "SRC  ZERO_7": {
+            "logo": "logos/src_zero_7.png",
+            "dados": [0, 0, null]
+          },
+          "SRC_Luciano☆": {
+            "logo": "logos/src_luciano.png",
+            "dados": [1, 0, 0]
+          },
+          "SRC  CH": {
+            "logo": "logos/src_ch.png",
+            "dados": [0, 0, null]
+          },
+          "SRC FA'BRÍC10": {
+            "logo": "logos/src_fabric10.png",
+            "dados": [null, 4, null]
+          }
+        },
+        "META GAMING": {
+          "《ŁĐ》YESHUA": {
+            "logo": "logos/yeshua.png",
+            "dados": [0, 2, 0]
+          },
+          "《ŁĐ》Lking⁷": {
+            "logo": "logos/lking7.png",
+            "dados": [0, 0, null]
+          },
+          "QI  SOUZA7": {
+            "logo": "logos/qi_souza7.png",
+            "dados": [2, 1, 0]
+          },
+          "B7  ENZINrlq": {
+            "logo": "logos/b7_enzinrlq.png",
+            "dados": [3, 7, 1]
+          }
+        },
+        "RJ _ E SPORTE": {
+          "RJ  criminoso": {
+            "logo": "logos/rj_criminoso.png",
+            "dados": [4, 4, 2]
+          },
+          "RJ  Lowzz⚔": {
+            "logo": "logos/rj_lowzz.png",
+            "dados": [4, 1, 3]
+          },
+          "RJ  DOGX": {
+            "logo": "logos/rj_dogx.png",
+            "dados": [1, null, 3]
+          },
+          "inumaki'": {
+            "logo": "logos/inumaki.png",
+            "dados": [3, 0, null]
+          },
+      "galaxyjota7": {
+        "logo": "logos/galaxyjota7.png",
+        "dados": [null, 5, 3]
+      }
+        },
+        "TEAM SX": {
+          "JOÃO  D7": {
+            "logo": "logos/joao_d7.png",
+            "dados": [1, 4, 0]
+          },
+          "GH  itachiϟ": {
+            "logo": "logos/gh_itachi.png",
+            "dados": [2, 3, 0]
+          },
+          "yunk  vino`": {
+          "logo": "logos/yunk_vino.png",
+          "dados": [1, 0, 0]
+          },
+          "K11  Silva7": {
+            "logo": "logos/k11_silva7.png",
+            "dados": [3, 2, 0]
+          }
+        },
+        "SS GAMING": {
+          "SS.DUQUE": {
+            "logo": "logos/ss_duque.png",
+            "dados": [4, 0, 0]
+          },
+          "SS  VINNI.": {
+            "logo": "logos/ss_vinni.png",
+            "dados": [4, 0, 0]
+          },
+          "WOD  SAGAZZ  z": {
+            "logo": "logos/wod_sagazz_z.png",
+            "dados": [1, 0, null]
+          },
+          "SS.JERRY¹⁵⁷": {
+            "logo": "logos/ss_jerry157.png",
+            "dados": [0, 0, null]
+          },
+          "SS.SHEIK1": {
+            "logo": "logos/ss_sheik1.png",
+            "dados": [null, null, 0]
+          }
+        },
+        "CPX GAMER BR": {
+
+          "CPX  SEREIA  †": {
+            "logo": "",
+              "dados": [0, 1, 2]
+          },
+          "CPX  fada  ⁰⁸⁴": {
+              "logo": "",
+              "dados": [0, 1, 1]
+          },
+          "CPX  RAXHA あ": {
+              "logo": "../../../assets/img/logosXtreinoEquipes/junho/(1.3)alliance.jpg",
+              "dados": [0, 4, 11]
+          },
+          "CPX  Ⓥ Z E U S ⬆": {
+              "logo": "",
+              "dados": [0, 7, 6]
+          }
+        },
+        "CERES FC": {
+          "REYY77⊗♧": {
+            "logo": "logos/reyy77.png",
+            "dados": [0, 0, 0]
+          },
+          "@Henrique.  么": {
+            "logo": "logos/henrique.png",
+            "dados": [0, 0, 0]
+          },
+          "i'm  ruim": {
+            "logo": "logos/im_ruim.png",
+            "dados": [0, 1, 2]
+          },
+          "彡 ANGOLA  ╰_╯": {
+            "logo": "logos/angola.png",
+            "dados": [0, 0, 0]
+          }
+        },
+        "ALLIANCE": {
+          "《UN》jhon": {
+            "logo": "logos/jhon.png",
+            "dados": [4, 1, 2]
+          },
+          "AC.Ligerimvp": {
+            "logo": "logos/ac_ligerimvp.png",
+            "dados": [1, 0, 1]
+          },
+          "Gabrielzxkkk": {
+            "logo": "logos/gabrielzxkkk.png",
+            "dados": [1, 0, 2]
+          },
+          "BRAINROT._♡♡": {
+            "logo": "logos/brainrot.png",
+            "dados": [2, 0, null]
+          },
+          "RX  ONE9": {
+            "logo": "logos/rx_one9.png",
+            "dados": [null, null, 4]
+          }
+        }
+      }
+    }
+}
+
+//equipe
+
+{
+    "treino1": {
+            "data": "10/06/2026",
+            "equipes": [
+                {
+                    "equipe": "ALLIANCE",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 8,
+                            "posicao": 1
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 1,
+                            "posicao": 8
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 9,
+                            "posicao": 6
+                        }
+                    ],
+                    "logo": "../../../assets/img/logosXtreinoEquipes/junho/(1.3)alliance.jpg",
+                    "puniçãoDescrição": "",
+                    "puniçãoPontos": ""
+                },
+                {
+                    "equipe": "RJ _ E SPORTE",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 12,
+                            "posicao": 2
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 10,
+                            "posicao": 2
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 11,
+                            "posicao": 2
+                        }
+                    ],
+                    "logo": "../../../assets/img/logosXtreinoEquipes/junho/(1.2)rj_e sport.jpg",
+                    "puniçãoDescrição": "",
+                    "puniçãoPontos": ""
+                },
+                {
+                    "equipe": "SS GAMING",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 9,
+                            "posicao": 3
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 0,
+                            "posicao": 9
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 0,
+                            "posicao": 9
+                        }
+                    ]
+                },
+                {
+                    "equipe": "SERROCITY",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 1,
+                            "posicao": 4
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 4,
+                            "posicao": 7
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 0,
+                            "posicao": 8
+                        }
+                    ]
+                },
+                {
+                    "equipe": "CPX GAMER BR",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 0,
+                            "posicao": 5
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 13,
+                            "posicao": 1
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 20,
+                            "posicao": 1
+                        }
+                    ],
+                    "logo": "../../../assets/img/logosXtreinoEquipes/junho/(1-1)cpxGamerBr.jpg",
+                    "puniçãoDescrição": "",
+                    "puniçãoPontos": ""
+                },
+                {
+                    "equipe": "TEAM SX",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 7,
+                            "posicao": 6
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 9,
+                            "posicao": 3
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 0,
+                            "posicao": 5
+                        }
+                    ]
+                },
+                {
+                    "equipe": "META GAMING",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 5,
+                            "posicao": 7
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 10,
+                            "posicao": 4
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 1,
+                            "posicao": 7
+                        }
+                    ]
+                },
+                {
+                    "equipe": "FCB E-SPORTS",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 1,
+                            "posicao": 8
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 2,
+                            "posicao": 5
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 5,
+                            "posicao": 3
+                        }
+                    ]
+                },
+                {
+                    "equipe": "CERES FC",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 0,
+                            "posicao": 9
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 1,
+                            "posicao": 6
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 2,
+                            "posicao": 4
+                        }
+                    ]
+                },
+                {
+                    "equipe": "REAL BALA",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 0,
+                            "posicao": 13
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 0,
+                            "posicao": 13
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 0,
+                            "posicao": 13
+                        }
+                    ]
+                },
+                {
+                    "equipe": "007 DRAGONS",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 0,
+                            "posicao": 13
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 0,
+                            "posicao": 13
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 0,
+                            "posicao": 13
+                        }
+                    ]
+                },
+                {
+                    "equipe": "FURY MOBILE GG",
+                    "detalhes": [
+                        {
+                            "queda": 1,
+                            "kills": 0,
+                            "posicao": 13
+                        },
+                        {
+                            "queda": 2,
+                            "kills": 0,
+                            "posicao": 13
+                        },
+                        {
+                            "queda": 3,
+                            "kills": 0,
+                            "posicao": 13
+                        }
+                    ]
+                }
+            ]
+        }
+}
